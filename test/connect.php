c87c39521551eec9ffc3185f49a8be33234a7ebc
Michelle Tan <mtan123456789@gmail.com>
2016-02-23 10:29:33 -0500

    following tutorial

--- test/connect.php
+++
@@ -1,2 +0,0 @@
-<?php
-$db = new mysqli('127.0.0.1','oot','189mk7E$E%NF#nc6','app');
---
+++ test/search.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Search Engine in PHP</title>
+	</head>
+	
+	<body>
+		<form action="search.php" method="get">
+			Search engine:<input type="text" name="value">
+			<input type="submit" name="search" value="Search Now">
+		</form>
+	</body>
+</html>
