759b615c5361e1f89b93081949863be71ecf8f9f
Michelle Tan <mtan123456789@gmail.com>
2016-05-10 11:19:44 -0400

    Kept working on searching algorithm

--- flaskapp/app/search.py
+++ flaskapp/app/search.py
@@ -1,16 +1,35 @@
-
+import Levenshtein
+import sys
 
 def get_language( search_value ):
+    dmin = sys.maxsize
+    lang = "None"
     with open("./static/txt/languages.txt") as f:
         for line in f:
-            if line.strip().lower() in search_value.strip().lower() :
-                return line.lower().strip()
+            if search_value is not None: 
+                for word in search_value.split( ):
+                    dis = Levenshtein.distance(line.strip().lower(),word.strip().lower())
+                    if dis < dmin:
+                        lang = line.strip().lower()
+                        dmin = dis
+        if dmin < 3:
+            return lang
         return None
-
+    
 def get_command( search_value ):
+    dmin = sys.maxsize
+    comm = "None"
     with open("./static/txt/commands.txt") as f:
         for line in f:
-            if line.strip().lower() in search_value.strip().lower():
-                return line.lower().strip()
+            if search_value is not None:
+                for word in search_value.split():
+                    dis = Levenshtein.distance(line.strip().lower(),word.strip().lower())
+                    print(line)
+                    print(word)
+                    if dis < dmin:
+                        comm = line.strip().lower()
+                        dmin = dis
+        if dmin < 5:
+            return comm
         return None
 
